05cf6277fc3626064876a0379c60f8bbc37b4aa8
AkinloluMubarakOpeyemi <oyemi7521@gmail.com>
2023-02-16 15:10:59 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *main- A program that print a line with put function
+ *Return: 0(Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multillingual puzzle");
+}
