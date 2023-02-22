6e8e02b06876f311b06158967c023f35f5b46691
AkinloluMubarakOpeyemi <oyemi7521@gmail.com>
2023-02-22 06:23:58 -0800

    k2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	while (x < 10)
         for (c = 'a'; c <= 'z'; c++)
         {
-                _putchar(c);
+                _putchar('c\n');
         }
         _putchar('\n');
 }
