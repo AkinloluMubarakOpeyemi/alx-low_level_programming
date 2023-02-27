b160dcff264816a24ea0143e11e9221919d224ef
AkinloluMubarakOpeyemi <oyemi7521@gmail.com>
2023-02-26 17:13:17 -0800

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,37 +1,51 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 0 times table, starting with 0
- * Return: empty output
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
  */
 
 void times_table(void)
-{
-	int x, y, z, u, d;
 
-	for (x = 0; x <= 9; x++)
-	{
-	for (y = 0; y <= 9; y++)
-	{
-	z = x + y;
-	if (z > 9)
-	{
-	u = z % 10;
-	d = (z - u) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(d + '0');
-	_putchar(u + '0');
-	}
-	else
-	{
-	if (y != 0)
-	}
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	_putchar(z + '0');
-	}
-	_putchar('\n');
+{
+
+int row, column, product, tens, ones;
+for (row = 0; row <= 9; row++)
+
+{
+
+for (column = 0; column <= 9; column++)
+
+{
+product = row * column;
+tens = product / 10;
+ones = product % 10;
+
+if (column == 0)
+{
+_putchar('0');
+}
+
+else if (product < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(ones + '0');
+																	}
+
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(tens + '0');
+_putchar(ones + '0');
+}
+
+}
+_putchar('\n');
+
+}
+
 }
