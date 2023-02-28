64b091b818e2ef76062ebcbed03d5e5c2595623b
AkinloluMubarakOpeyemi <oyemi7521@gmail.com>
2023-02-28 05:16:56 -0800

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - return strlen
+ * @s: argument
+ * Return: length of str
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s != '\0'; x++)
+	{
+		s++;
+	}
+	return (x);
+}
