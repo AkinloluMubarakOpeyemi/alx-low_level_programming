270b011ab4a1ee3092698a0b8eed60d7ae559969
AkinloluMubarakOpeyemi <oyemi7521@gmail.com>
2023-02-28 15:46:16 -0800

    task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * *_strcpy - copies pointer to a str
+ * @dest: first pointer
+ * @src: second pointer
+ * Return: 0
  */
-int main(void)
-{
-	char s1[98];
-	char *ptr;
 
-	ptr = _strcpy(s1, "First, solve the problem. then, write the code\n");
-	printf("%s", s1);
-	printf("%s", ptr);
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
 	return (0);
 }
